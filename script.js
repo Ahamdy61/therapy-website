f0b70b687bfdcf67b7106d320b7f0bd872a8b88e
Ahamdy61 <hamdyemad456@gmail.com>
2026-08-10 21:25:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector("nav ul");
 
+if (menuToggle && navMenu) {
+
+    menuToggle.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+    });
+
+    document.querySelectorAll("nav ul a").forEach(link => {
+        link.addEventListener("click", () => {
+            navMenu.classList.remove("active");
+        });
+    });
+
+}
